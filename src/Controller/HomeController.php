fccee8fe801a7d9117f47c7b173ed9908f5278e7
Florian Louvet <florian.louvet@epitech.eu>
2019-11-30 11:48:38 +0100

    [+] Added alert and timer templates

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,4 +16,22 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+    /**
+     * @Route("/alert", name="alert")
+     */
+    public function alert()
+    {
+        return $this->render('alert/index.html.twig', [
+            'controller_name' => 'Alert',
+        ]);
+    }
+    /**
+     * @Route("/timer", name="timer")
+     */
+    public function timer()
+    {
+        return $this->render('timer/index.html.twig', [
+            'controller_name' => 'Timer',
+        ]);
+    }
 }
