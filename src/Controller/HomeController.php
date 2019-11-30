d2f527f7682cfd499dc2096926c659e9ca59298d
Florian Louvet <florian.louvet@epitech.eu>
2019-11-30 12:11:04 +0100

    [~] Added correct icons to navbar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,26 +12,63 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render(
+            'home/index.html.twig',
+            [
+                'controller_name' => 'HomeController',
+            ]
+        );
     }
+
     /**
      * @Route("/alert", name="alert")
      */
     public function alert()
     {
-        return $this->render('alert/index.html.twig', [
-            'controller_name' => 'Alert',
-        ]);
+        return $this->render(
+            'alert/index.html.twig',
+            [
+                'controller_name' => 'Alert',
+            ]
+        );
     }
+
     /**
      * @Route("/timer", name="timer")
      */
     public function timer()
     {
-        return $this->render('timer/index.html.twig', [
-            'controller_name' => 'Timer',
-        ]);
+        return $this->render(
+            'timer/index.html.twig',
+            [
+                'controller_name' => 'Timer',
+            ]
+        );
     }
+
+    /**
+     * @Route("/calendar", name="calendar")
+     */
+    public function calendar()
+    {
+        return $this->render(
+            'calendar/index.html.twig',
+            [
+                'controller_name' => 'Calendar',
+            ]
+        );
+    }
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function profile()
+    {
+        return $this->render(
+            'profile/index.html.twig',
+            [
+                'controller_name' => 'Profile',
+            ]
+        );
+    }
+
 }
