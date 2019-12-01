790ad3074a4989cd3dd4a13279a19ce76e8a4c71
Florian Louvet <florian.louvet@epitech.eu>
2019-12-01 10:37:16 +0100

    [~] Fixed some errors

--- js/calendar-page.js
+++ js/calendar-page.js
@@ -3,7 +3,7 @@ $('#example2').calendar({
     inline: true,
     text: {
         days: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
-        months: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre'],
+        months: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
         monthsShort: ['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jui', 'Juil', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec'],
         today: 'Aujourd\'hui',
         now: 'Maintenant',
